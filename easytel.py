ad5666dc023076e5e389113d0cdf9315a24803aa
Nash Reilly <Nrc.reilly@gmail.com>
2014-03-12 16:56:13 -0400

    Update easytel.py

--- easytel.py
+++ easytel.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-#  Copyright (c) 2005, Corey Goldberg
-#
-#  TelnetController.py is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
- 
-
 from telnetcontroller import TelnetController, ConnectionFailed
 import argparse
 import getpass
